088bb44185ae35022397a00be58069c9ac65b8d5
Tessabaxter12 <tessabaxter12@gmail.com>
2023-11-29 09:03:33 -0800

    Resolve Merge Conflict

---
+++ js/main.js
@@ -0,0 +1,22 @@
+console.log("linked And Ready")
+
+//constants, Cards images of animals
+
+//cached element references
+
+//initial variables
+
+
+//call random function for sorting cards onto 4x4 grid
+//player one goes first
+//Need to display Wrong guess until a time expires or next click
+//selects two cards, they flip over, pointer no longer works
+//need a function to determine pair, cards go away, pair gets add to that players total pairs, take anouther turn
+//need a function to determine no match, turn over
+//loop break if too much time goes by or after too many bad guesse
+//need a function to rotate to next player, repeat
+
+//renderResults
+//cards finished, count pairs, declair a winner
+//need a getwinner function
+//option to play again
