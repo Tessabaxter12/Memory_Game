22eaff0ea94954bf05370bafb03185e5bf4c2f96
Tessabaxter12 <tessabaxter12@gmail.com>
2023-12-02 04:01:39 -0800

    day 2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-class CountdownTimer {
-    constructor({
-        selector,
-        targetTime
-    })  {
-        this.selector = selector
-        this.targeTime = targetTime
-        }
+const startingMinutes = 5;
+let time = startingMinutes * 60;
 
-    getTimeRemaining(endtime) {
-        console.log(endtime)
-    }
+const countdownEL = document.getElementById('countdown');
 
-    updateTimer({ minutes, seconds }) {
+setInterval(updateCountdown, 1000);
 
-    }
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-    startTimer() {
-        const timer = this.getTimeRemaining(this.targetTime)
-    }
+    seconds = seconds < 5 ? '0' + seconds : seconds;
+
+    countdownEL.innerHTML = '${minutes}:${seconds}'; time--;
 }
 
-const timer = new CountdownTimer({
-    selector: 'clock',
-    targetTime: ('5:00')   
-})
-timer.startTimer()
+
+// let countdownTime = new Time().setMinutes(new Time().getMinutes() + 5)
+
+// let timeInterval
+//     const minutesElem = document.querySelector('#minutes'),
+//         secondsElem = document.querySelector('#seconds'),
+//         timerRunningContent = document.querySelector('#playbutton'),
+//         timerEndContent = document.querySelector('#timer-end')
+
+// const startCountdown = () => {
+//         const now = new Time().getTime()
+//         const countdown = new Time(countdownTime).getTime()
+//         const difference = (countdown - now) / 1000
+//         let minutes = math.floor((difference % 60))
+//         let seconds = Math.floor(difference %)
+//     }
