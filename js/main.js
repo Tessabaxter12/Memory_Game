7dfcf62d486f1ec26b582a31c7d0bef1ec0332f3
Tessabaxter12 <tessabaxter12@gmail.com>
2023-12-05 20:56:30 -0800

    Cards Matching

--- js/main.js
+++ js/main.js
@@ -1,79 +1,76 @@
 //array = cards
 //change image to fImage(unique) then add bImage(same) in all of the card objects 
-//Add the property fliped: false to all of the object
+//Add the property flipped: false to all of the object
 //add a check for match function that looks at the card name of the first click and compares to the card name of the second clicked
 //if they are the same keep them flipped and changed matched to true for both
 //if they are not the same changed flipped back to false and display the bImage instead of the fImage
-//on each click assign the first card1selected and card2selected
-
-
+//on each click assign the first card1selected and card2selecte
 
 //Define constants
 const gameBoard = document.querySelectorAll(".gameBoard > div");
 const results = document.getElementById("results");
 const startButton = document.getElementById("startGame");
+const backImage = '../bImages/Fishbackground.avif'
 
 //Variables
 let gameOn = false;
-let counter = 0;
-let card1Selected = "";
-let card2Selected = "";
-
+let card1;
+let card2;
 
 //Array of images of card fronts
-const cardFronts = [
-  { name: "Dolphin", image: "images/Dolphin.png", matched: false},
-  { name: "Crab", image: "images/Crab.jpeg", matched: false},
-  { name: "Stingray", image: "images/Stingray.webp", matched: false},
-  { name: "Turtle", image: "images/Turtle.webp", matched: false},
-  { name: "Star Fish", image: "images/Star Fish.avif", matched: false}, 
-  { name: "Fish", image: "images/Fish.avif", matched: false},
-  { name: "Sea Horse", image: "images/Sea Horse.avif", matched: false},
-  { name: "JellyFish",  image: "images/JellyFish.avif", matched: false},
-  { name: "Star Fish", image: "images/Star Fish.avif", matched: false},
-  { name: "Dolphin", image: "images/Dolphin.png", matched: false},
-  { name: "Octopus", image: "images/Octopus.avif", matched: false},
-  { name: "Fish", image: "images/Fish.avif", matched: false},
-  { name: "Sea Horse", image: "images/Sea Horse.avif", matched: false},
-  { name: "Turtle", image: "images/Turtle.webp", matched: false},
-  { name: "Octopus", image: "images/Octopus.avif", matched: false},
-  { name: "JellyFish",  image: "images/JellyFish.avif", matched: false},
-  { name: "Stingray", image: "images/Stingray.webp", matched: false},
-  { name: "Crab", image: "images/Crab.jpeg", matched: false}
+const cards = [
+  { name: "Dolphin", fImage: "fImages/Dolphin.png", matched: false, flipped: false},
+  { name: "Crab", fImage: "fImages/Crab.jpeg", matched: false, flipped: false},
+  { name: "Stingray", fImage: "fImages/Stingray.webp", matched: false, flipped: false},
+  { name: "Turtle", fImage: "fImages/Turtle.webp", matched: false, flipped: false},
+  { name: "Star Fish", fImage: "fImages/Star Fish.avif", matched: false, flipped: false}, 
+  { name: "Fish", fImage: "fImages/Fish.avif", matched: false, flipped: false},
+  { name: "Sea Horse", fImage: "fImages/Sea Horse.avif", matched: false, flipped: false},
+  { name: "JellyFish",  fImage: "fImages/JellyFish.avif", matched: false, flipped: false},
+  { name: "Star Fish", fImage: "fImages/Star Fish.avif", matched: false, flipped: false},
+  { name: "Dolphin", fImage: "fImages/Dolphin.png", matched: false, flipped: false},
+  { name: "Octopus", fImage: "fImages/Octopus.avif", matched: false, flipped: false},
+  { name: "Fish", fImage: "fImages/Fish.avif", matched: false, flipped: false},
+  { name: "Sea Horse", fImage: "fImages/Sea Horse.avif", matched: false, flipped: false},
+  { name: "Turtle", fImage: "fImages/Turtle.webp", matched: false, flipped: false},
+  { name: "Octopus", fImage: "fImages/Octopus.avif", matched: false, flipped: false},
+  { name: "JellyFish",  fImage: "fImages/JellyFish.avif", matched: false, flipped: false},
+  { name: "Stingray", fImage: "fImages/Stingray.webp", matched: false, flipped: false},
+  { name: "Crab", fImage: "fImages/Crab.jpeg", matched: false, flipped: false}
 ];
 
 //Intializing the game
 function init() {
   gameOn = true
   timer()
-  //  shuffle ()
+//  shuffle()
 }
 
 //function to shuffle the card fronts
-// function shuffleCards = (cardFronts) {
+// function shuffleCards = (cards){
 //   let currentIndex = array.length;
 //   let temporaryValue, randomIndex;
-   //Shuffle elements
+//   //Shuffle elements
 //   while (0 !== currentIndex) {
-       //Pick the remaining element
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex -= 1;
-       // And swap it with the current element.
-//       temporaryValue = cardFront[currentIndex];
-//       cardFront[currentIndex] = cardFont[randomIndex];
-//       cardFront[randomIndex] = temporaryValue;
-//   }
-//   return cardFronts;
+//     //Pick the remaining element
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+//     // And swap it with the current element.
+//     temporaryValue = cardFront[currentIndex];
+//     cardFront[currentIndex] = cardFont[randomIndex];
+//     cardFront[randomIndex] = temporaryValue;
+// }
+//   return cards;
 // }
 
 //User clicks the start game button
 //Event Listener
 startButton.addEventListener("click",init)
-
 gameBoard.forEach(function(card,i){
+  cards[i].element=gameBoard[i]
   card.addEventListener("click",function(e){
     //Front of card flips over
-    handleMove(e,cardFronts[i])
+    handleMove(e,cards[i])
     //Starts timer when first card flipped
   })
 })
@@ -92,39 +89,56 @@ function timer(){
 
 //Tallys Wrong Moves
 
-//handle move allows the back of cards to be clicked
+//Handle Move Function allows the back of cards to be clicked
 function handleMove(event,card){
   if (gameOn){
-    //  console.log(event.target.style)
-    console.log("the card clicked", event.target)
-    console.log("this should be the card", card)
-    event.target.style.backgroundImage = `url("../${card.image}")`
-  }else{
-    // NOTE - SEND A MESSAGE TO USER TO HIT THE START BUTTON
-    return
-  }
-  
+    
+    if (!card1){
+      card1 = card
+    } else if (card1){
+      card2 = card
+    } 
 
+    //Allows user to flip the cards 
+    event.target.style.backgroundImage = `url("../${card.fImage}")`
+    checkMatch ()
+    card.flipped = true
+    } else {
+      return
+      //Must hit the start button to begin the game.
+    }
+    console.log(card1)
+    console.log(card2)
+  }
+
+//Check 2 cards to see if they match
+function checkMatch (){
+  if (card1?.name === card2?.name) {
+    card1.match = true 
+    card2.match = true
+    card1 = undefined
+    card2 = undefined
+  } else if(card1 && card2) {
+    console.log(card1,card2)
+    card1.element.style.backgroundImage=`url("${backImage}")`
+    card2.element.style.backgroundImage=`url("${backImage}")`
+    card1 = undefined
+    card2 = undefined
+  } 
 }
 
-//Function player selects two cards, they flip over, pointer no longer works
-// function select2Cards() {
-//     if (counter === 0){
-// //     let card1Selected = "";
-// //     let card2Selected = "";
-// //   }
-// //   else {
-//         this.card2Selected
-// //   }
-// // }
 
-// Function to determine pair
 
-// Else function to determine no match, cards flip back over, and take another turn
-//const noMatch = function(card1Selected, card2Selected) {
 
-//}
+
+
+
 // Render Results When
+// function renderMessage() {
+//   if (cards === flipped) && (timer > 0) {MessageChannel.innerText = "You Win"}
+//   else if(cards ! flipped) && (timer === 0) {MessageChannel.innerText = "You loose"}
+//   }
+// }
 //Time out
 //No cards left on board
 //Declair you win or you loose
